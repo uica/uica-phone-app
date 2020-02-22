55092de42c910a2d5ae18a6264b277992155b086
Muhammad Al Juburi <maljuburi89@gmail.com>
2020-02-22 13:07:31 -0700

    added stripe packages and form for payments

--- components/Screens/Donation.js
+++ components/Screens/Donation.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import Payment from "../Payment/Payment";
 const Donation = props => {
   return (
-    <View>
-      <Text>Make donations</Text>
+    <View style={styles.donationContainer}>
+      <Text style={styles.formTitle}>Make donations</Text>
+      <Payment />
     </View>
   );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  donationContainer: {
+    padding: 10
+  },
+  formTitle: {
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 10
+  }
+});
 export default Donation;
