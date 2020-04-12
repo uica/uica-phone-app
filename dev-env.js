bedaa284e9ac4f68571a9221c8ce1666b1171178
Muhammad Al Juburi <maljuburi89@gmail.com>
2020-04-12 16:44:28 -0700

    updated dev-env

--- dev-env.js
+++ dev-env.js
@@ -5,9 +5,6 @@
  ******************************/
 
 import Constants from "expo-constants";
-import { Platform } from "react-native";
-
-const localhost = Platform.OS === "ios" ? "localhost:8080" : "10.0.2.2:8080";
 
 const ENV = {
   development: {
